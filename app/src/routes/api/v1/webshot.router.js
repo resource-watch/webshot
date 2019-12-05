54e6eff386a548192886870d4fb2e4ba58c49602
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-05 11:55:10 +0000

    Fix coding style after updating eslint config

--- app/src/routes/api/v1/webshot.router.js
+++ app/src/routes/api/v1/webshot.router.js
@@ -9,130 +9,130 @@ const S3Service = require('services/s3.service');
 const config = require('config');
 
 const router = new Router({
-  prefix: '/webshot',
+    prefix: '/webshot',
 });
 
 const viewportDefaultOptions = { width: 1024, height: 768, isMobile: true };
 const gotoOptions = { waitUntil: 'networkidle' };
 
 const getDelayParam = (param) => {
-  const n = parseInt(param, 10);
-  if (typeof n === 'number' && !isNaN(n)) return n;
-  return param;
+    const n = parseInt(param, 10);
+    if (typeof n === 'number' && !isNaN(n)) return n;
+    return param;
 };
 
 class WebshotRouter {
 
-  static async removeFolder(path) {
-    return new Promise((resolve, reject) => {
-      rimraf(path, (err) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve();
-      });
-    });
-  }
-
-  static async screenshot(ctx) {
-    ctx.assert(ctx.query.url, 400, 'url param is required');
-    ctx.assert(ctx.query.filename, 400, 'filename param required');
-    logger.info(`Doing screenshot of ${ctx.query.url}`);
-
-    // Validating URL
-    const urlObject = url.parse(ctx.query.url);
-    ctx.assert(/http|https/.test(urlObject.protocol), 400, 'The protocol in url param is not valid. Use http or https.');
-
-    const viewportOptions = Object.assign({}, viewportDefaultOptions);
-    const tmpDir = tmp.dirSync();
-    const filename = `${ctx.query.filename}-${Date.now()}.pdf`;
-    const filePath = `${tmpDir.name}/${filename}`;
-    const delay = getDelayParam(ctx.query.waitFor);
-
-    if (ctx.query.landscape && ctx.query.landscape === 'true') viewportOptions.isLandscape = true;
-    if (ctx.query.width) viewportOptions.width = parseInt(ctx.query.width, 10);
-    if (ctx.query.height) viewportOptions.height = parseInt(ctx.query.height, 10);
-
-    try {
-      logger.debug(`Saving in: ${filePath}`);
-
-      // Using Puppeteer
-      const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-      const page = await browser.newPage();
-      await page.setViewport(viewportOptions);
-      await page.goto(ctx.query.url, gotoOptions);
-      if (delay) await page.waitFor(delay);
-      if (ctx.query.mediatype) await page.emulateMedia(ctx.query.mediatype);
-
-      // Whether or not to include the background
-      const printBackground = !!(ctx.query.backgrounds && ctx.query.backgrounds === 'true');
-
-      await page.pdf({ path: filePath, format: 'A4', printBackground });
-
-      browser.close();
-
-      // Sending file to download
-      ctx.set('Content-disposition', `attachment; filename=${filename}`);
-      ctx.set('Content-type', 'application/pdf');
-      await send(ctx, filePath, { root: '/' });
-    } catch (err) {
-      logger.error(err);
+    static async removeFolder(path) {
+        return new Promise((resolve, reject) => {
+            rimraf(path, (err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        });
     }
-  }
 
-  static async widgetThumbnail(ctx) {
-    const widget = ctx.params.widget;
-    logger.info(`Generating thumbnail for widget ${widget}`);
+    static async screenshot(ctx) {
+        ctx.assert(ctx.query.url, 400, 'url param is required');
+        ctx.assert(ctx.query.filename, 400, 'filename param required');
+        logger.info(`Doing screenshot of ${ctx.query.url}`);
 
-    // Validating URL
-    const viewportOptions = Object.assign({}, viewportDefaultOptions);
-    const tmpDir = tmp.dirSync();
-    const filename = `${widget}-${Date.now()}.png`;
-    const filePath = `${tmpDir.name}/${filename}`;
-    const renderUrl = `${config.get('service.appUrl')}${widget}`;
+        // Validating URL
+        const urlObject = url.parse(ctx.query.url);
+        ctx.assert(/http|https/.test(urlObject.protocol), 400, 'The protocol in url param is not valid. Use http or https.');
 
-    if (ctx.query.width) viewportOptions.width = parseInt(ctx.query.width, 10);
-    if (ctx.query.height) viewportOptions.height = parseInt(ctx.query.height, 10);
+        const viewportOptions = Object.assign({}, viewportDefaultOptions);
+        const tmpDir = tmp.dirSync();
+        const filename = `${ctx.query.filename}-${Date.now()}.pdf`;
+        const filePath = `${tmpDir.name}/${filename}`;
+        const delay = getDelayParam(ctx.query.waitFor);
 
-    logger.info(`Capturing URL: ${renderUrl}`);
+        if (ctx.query.landscape && ctx.query.landscape === 'true') viewportOptions.isLandscape = true;
+        if (ctx.query.width) viewportOptions.width = parseInt(ctx.query.width, 10);
+        if (ctx.query.height) viewportOptions.height = parseInt(ctx.query.height, 10);
 
-    try {
-      logger.debug(`Saving in: ${filePath}`);
+        try {
+            logger.debug(`Saving in: ${filePath}`);
 
-      // Using Puppeteer
-      await puppeteer.launch({ args: ['--no-sandbox'] }).then(async browser => {
-        const page = await browser.newPage();
+            // Using Puppeteer
+            const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+            const page = await browser.newPage();
+            await page.setViewport(viewportOptions);
+            await page.goto(ctx.query.url, gotoOptions);
+            if (delay) await page.waitFor(delay);
+            if (ctx.query.mediatype) await page.emulateMedia(ctx.query.mediatype);
 
-        await page.setViewport(viewportOptions);
-        await page.goto(renderUrl, { waitUntil: ['networkidle2', 'domcontentloaded'] });
+            // Whether or not to include the background
+            const printBackground = !!(ctx.query.backgrounds && ctx.query.backgrounds === 'true');
 
-        await page.waitFor(
-          () => window.WEBSHOT_READY,
-          { timeout: 30000 }
-        )
-          .then(async () => {
-            // TODO: this is a hack to account for the fact that the <canvas> is added to the DOM but takes a bit to actually render
-            await page.waitFor(1000);
-
-            const element = await page.$('.widget-content');
-            await element.screenshot({ path: filePath });
-
-            const s3file = await S3Service.uploadFileToS3(filePath, filename);
-            ctx.body = { data: { widgetThumbnail: s3file } };
+            await page.pdf({ path: filePath, format: 'A4', printBackground });
 
             browser.close();
-          }, (error) => {
-            logger.error(`Could not render screenshot for widget ${widget}: ${error}`);
-            throw error;
-          });
-      });
 
-    } catch (err) {
-      logger.error(err);
-      throw err;
+            // Sending file to download
+            ctx.set('Content-disposition', `attachment; filename=${filename}`);
+            ctx.set('Content-type', 'application/pdf');
+            await send(ctx, filePath, { root: '/' });
+        } catch (err) {
+            logger.error(err);
+        }
     }
-  }
+
+    static async widgetThumbnail(ctx) {
+        const widget = ctx.params.widget;
+        logger.info(`Generating thumbnail for widget ${widget}`);
+
+        // Validating URL
+        const viewportOptions = Object.assign({}, viewportDefaultOptions);
+        const tmpDir = tmp.dirSync();
+        const filename = `${widget}-${Date.now()}.png`;
+        const filePath = `${tmpDir.name}/${filename}`;
+        const renderUrl = `${config.get('service.appUrl')}${widget}`;
+
+        if (ctx.query.width) viewportOptions.width = parseInt(ctx.query.width, 10);
+        if (ctx.query.height) viewportOptions.height = parseInt(ctx.query.height, 10);
+
+        logger.info(`Capturing URL: ${renderUrl}`);
+
+        try {
+            logger.debug(`Saving in: ${filePath}`);
+
+            // Using Puppeteer
+            await puppeteer.launch({ args: ['--no-sandbox'] }).then(async browser => {
+                const page = await browser.newPage();
+
+                await page.setViewport(viewportOptions);
+                await page.goto(renderUrl, { waitUntil: ['networkidle2', 'domcontentloaded'] });
+
+                await page.waitFor(
+                    () => window.WEBSHOT_READY,
+                    { timeout: 30000 }
+                )
+                    .then(async () => {
+                        // TODO: this is a hack to account for the fact that the <canvas> is added to the DOM but takes a bit to actually render
+                        await page.waitFor(1000);
+
+                        const element = await page.$('.widget-content');
+                        await element.screenshot({ path: filePath });
+
+                        const s3file = await S3Service.uploadFileToS3(filePath, filename);
+                        ctx.body = { data: { widgetThumbnail: s3file } };
+
+                        browser.close();
+                    }, (error) => {
+                        logger.error(`Could not render screenshot for widget ${widget}: ${error}`);
+                        throw error;
+                    });
+            });
+
+        } catch (err) {
+            logger.error(err);
+            throw err;
+        }
+    }
 
 }
 
--- app/test/e2e/service.spec.js
+++ app/test/e2e/service.spec.js
@@ -1,22 +1,21 @@
-const logger = require('logger');
+/* eslint-disable import/no-extraneous-dependencies */
 const nock = require('nock');
-const request = require('superagent').agent();
-const BASE_URL = require('./test.constants').BASE_URL;
+
 require('should');
 
 describe('E2E test', () => {
 
-  before(() => {
+    before(() => {
 
-    // simulating gateway communications
-    nock(`${process.env.CT_URL}/v1`)
-      .post('/', () => true)
-      .reply(200, {
-        status: 200,
-        detail: 'Ok'
-      });
-  });
+        // simulating gateway communications
+        nock(`${process.env.CT_URL}/v1`)
+            .post('/', () => true)
+            .reply(200, {
+                status: 200,
+                detail: 'Ok'
+            });
+    });
 
-  after(() => {
-  });
+    after(() => {
+    });
 });
