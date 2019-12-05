7bf38c87ab594c0ba81d7b2ddf90f9f64a4b5873
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-05 11:55:10 +0000

    Fix coding style after updating eslint config

--- app/src/app.js
+++ app/src/app.js
@@ -9,10 +9,10 @@ const ErrorSerializer = require('serializers/error.serializer');
 require('dotenv').config();
 
 const koaBody = require('koa-body')({
-  multipart: true,
-  jsonLimit: '50mb',
-  formLimit: '50mb',
-  textLimit: '50mb'
+    multipart: true,
+    jsonLimit: '50mb',
+    formLimit: '50mb',
+    textLimit: '50mb'
 });
 
 const app = new Koa();
@@ -20,23 +20,23 @@ const app = new Koa();
 app.use(convert(koaBody));
 
 app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    let error = err;
     try {
-      error = JSON.parse(err);
-    } catch (e) {
-      logger.error('Error parse');
+        await next();
+    } catch (err) {
+        let error = err;
+        try {
+            error = JSON.parse(err);
+        } catch (e) {
+            logger.error('Error parse');
+        }
+        ctx.status = error.status || 500;
+        logger.error(error);
+        ctx.body = ErrorSerializer.serializeError(ctx.status, error.message);
+        if (process.env.NODE_ENV === 'prod' && this.status === 500) {
+            ctx.body = 'Unexpected error';
+        }
+        ctx.response.type = 'application/vnd.api+json';
     }
-    ctx.status = error.status || 500;
-    logger.error(error);
-    ctx.body = ErrorSerializer.serializeError(ctx.status, error.message);
-    if (process.env.NODE_ENV === 'prod' && this.status === 500) {
-      ctx.body = 'Unexpected error';
-    }
-    ctx.response.type = 'application/vnd.api+json';
-  }
 
 });
 
@@ -45,25 +45,25 @@ app.use(koaLogger());
 loader.loadRoutes(app);
 
 const instance = app.listen(process.env.PORT, () => {
-  if (process.env.CT_URL) {
-    ctRegisterMicroservice.register({
-      info: require('../microservice/register.json'),
-      swagger: require('../microservice/public-swagger.json'),
-      mode: (process.env.CT_REGISTER_MODE && process.env.CT_REGISTER_MODE === 'auto') ? ctRegisterMicroservice.MODE_AUTOREGISTER : ctRegisterMicroservice.MODE_NORMAL,
-      framework: ctRegisterMicroservice.KOA2,
-      app,
-      logger,
-      name: config.get('service.name'),
-      ctUrl: process.env.CT_URL,
-      url: process.env.LOCAL_URL,
-      token: process.env.CT_TOKEN,
-      active: true,
-    }).then(() => {
-    }, (err) => {
-      logger.error(err);
-      process.exit(1);
-    });
-  }
+    if (process.env.CT_URL) {
+        ctRegisterMicroservice.register({
+            info: require('../microservice/register.json'),
+            swagger: require('../microservice/public-swagger.json'),
+            mode: (process.env.CT_REGISTER_MODE && process.env.CT_REGISTER_MODE === 'auto') ? ctRegisterMicroservice.MODE_AUTOREGISTER : ctRegisterMicroservice.MODE_NORMAL,
+            framework: ctRegisterMicroservice.KOA2,
+            app,
+            logger,
+            name: config.get('service.name'),
+            ctUrl: process.env.CT_URL,
+            url: process.env.LOCAL_URL,
+            token: process.env.CT_TOKEN,
+            active: true,
+        }).then(() => {
+        }, (err) => {
+            logger.error(err);
+            process.exit(1);
+        });
+    }
 });
 
 module.exports = instance;
