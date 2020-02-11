8977b9ec06cd93886dc8450704e75bcf8940de6e
Jesús González <jesus198927@gmail.com>
2020-02-11 16:43:50 +0100

    Remove current open server to permit the stub of koa-send

--- app/test/e2e/utils/test-server.js
+++ app/test/e2e/utils/test-server.js
@@ -2,21 +2,14 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const nock = require('nock');
 
-let requester;
-
 chai.use(chaiHttp);
 
 exports.getTestServer = async function getTestServer() {
-    if (requester) {
-        return requester;
-    }
-
     nock(process.env.CT_URL)
         .post(`/api/v1/microservice`)
         .reply(200);
 
     const serverPromise = require('../../../src/app');
     const { server } = await serverPromise();
-    requester = chai.request(server).keepOpen();
-    return requester;
+    return chai.request(server).keepOpen();
 };
