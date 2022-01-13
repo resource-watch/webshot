dbae152ac8d25162f010c6e82798196d7fd6f316
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2022-01-13 14:20:45 +0100

    adds layer thumbnail endpoint + tests

---
+++ app/test/e2e/layer-thumbnail-post.spec.js
@@ -0,0 +1,66 @@
+const chai = require('chai');
+const nock = require('nock');
+const sinon = require('sinon');
+
+const { getTestServer } = require('./utils/test-server');
+const { stubPuppeteer, stubS3 } = require('./utils/stubs');
+
+chai.should();
+
+let requester;
+let sinonSandbox;
+
+nock.disableNetConnect();
+nock.enableNetConnect(process.env.HOST_IP);
+
+describe('POST layer/:layer/thumbnail', () => {
+    before(async () => {
+        if (process.env.NODE_ENV !== 'test') {
+            throw Error(`Running the test suite with NODE_ENV ${process.env.NODE_ENV} may result in permanent data loss. Please use NODE_ENV=test.`);
+        }
+
+        requester = await getTestServer();
+    });
+
+    beforeEach(() => { sinonSandbox = sinon.createSandbox(); });
+
+    it('Takes a snapshot of the layer returning 200 OK with the URL for the screenshot (happy case)', async () => {
+        stubPuppeteer(sinonSandbox, 'https://resourcewatch.org/webshot/layer/123?');
+        stubS3(sinonSandbox, 'http://www.example.com');
+
+        const response = await requester.post(`/api/v1/webshot/layer/123/thumbnail`).send();
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('object');
+        response.body.data.should.have.property('layerThumbnail').and.equal('http://www.example.com');
+    });
+
+    it('If puppeteer fails taking the screenshot returns 400 Internal Server Error', async () => {
+        stubPuppeteer(sinonSandbox, false);
+        stubS3(sinonSandbox, 'http://www.example.com');
+
+        const response = await requester.post(`/api/v1/webshot/layer/123/thumbnail`).send();
+        response.status.should.equal(400);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('status').and.equal(400);
+        response.body.errors[0].should.have.property('detail').and.include('Error taking screenshot on URL');
+    });
+
+    it('If uploading to S3 fails returns 500 Internal Server Error', async () => {
+        stubPuppeteer(sinonSandbox, 'https://resourcewatch.org/webshot/layer/123');
+        stubS3(sinonSandbox, 'http://www.example.com', false);
+
+        const response = await requester.post(`/api/v1/webshot/layer/123/thumbnail`).send();
+        response.status.should.equal(500);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('status').and.equal(500);
+        response.body.errors[0].should.have.property('detail').and.include('Error uploading screenshot to S3');
+    });
+
+    afterEach(() => {
+        if (!nock.isDone()) {
+            throw new Error(`Not all nock interceptors were used: ${nock.pendingMocks()}`);
+        }
+
+        sinonSandbox.restore();
+    });
+});
